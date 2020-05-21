6dc9313b3643fbf210024d5deaddc4498545e78c
ahomeniuk <andriy.homeniuk@gmail.com>
2020-05-21 08:25:00 +0300

    update db

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM tiangolo/uwsgi-nginx-flask:python3.6-alpine3.7
 RUN apk --update add bash nano
 
 ENV STATIC_URL /static
-ENV STATIC_PATH /project/static
+ENV STATIC_PATH /app/static
 
 COPY requirements.txt requirements.txt
 
--- app/config.py
+++ app/config.py
@@ -1,18 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRES_PASSWORD']
-host = os.environ['POSTGRES_HOST']
-database = os.environ['POSTGRES_DB']
-port = os.environ['POSTGRES_PORT']
-
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
-    DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
