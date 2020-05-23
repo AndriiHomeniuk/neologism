00a3328a8827676c5ed7cf782a6d2ea03447abb1
ahomeniuk <andriy.homeniuk@gmail.com>
2020-05-23 13:03:11 +0300

    update searching process

--- app/search_neologism_logic.py
+++ app/search_neologism_logic.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import sqlite3
 import urllib.error
 import urllib.request
@@ -7,59 +8,44 @@ from urllib.parse import quote
 from bs4 import BeautifulSoup
 
 TRANSLIT = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "г": "h",
-    "ґ": "g",
-    "д": "d",
-    "е": "e",
-    "є": "ie",
-    "ж": "zh",
-    "з": "z",
-    "и": "y",
-    "і": "i",
-    "ї": "i",
-    "й": "i",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "kh",
-    "ц": "ts",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "shch",
-    "ю": "iu",
-    "я": "ia",
-    "ь": "j"
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'h',
+    'ґ': 'g',
+    'д': 'd',
+    'е': 'e',
+    'є': 'ie',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'y',
+    'і': 'i',
+    'ї': 'i',
+    'й': 'i',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'kh',
+    'ц': 'ts',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'shch',
+    'ю': 'iu',
+    'я': 'ia',
+    'ь': 'j'
 }
 
 
-def open_file():
-    with open('../neologism_file.txt', 'r', encoding="UTF-8") as read_file:
-        text = read_file.read()
-    return text
-
-
 def text_to_list(text):
-    """
-    This function help split text to list for next process.
-    """
-    text_list = []
-    for word in text.split(' '):
-        if word:
-            if not word[-1].isalpha():
-                word = word[:-1]
-            text_list.append(word.lower())
-    return list(set(text_list))
+    return list(set(re.findall(r'\w+', text)))
 
 
 def search_words_own_vocabulary(text_list):
@@ -67,14 +53,12 @@ def search_words_own_vocabulary(text_list):
     This function search neologisms from the text on the own vocabularies and give list of neologisms.
     """
     pre_list_of_neologisms = []
-    vocabulary = sqlite3.connect("voc.db")
+    vocabulary = sqlite3.connect('../voc.db')
     voc = vocabulary.cursor()
     for words in text_list:
         voc.execute("SELECT word FROM voc WHERE word='%s'" % words)
         result = voc.fetchall()
-        if not result:
-            pre_list_of_neologisms.append(words)
-        elif words not in result[0][0]:
+        if not result or words not in result[0][0]:
             pre_list_of_neologisms.append(words)
     voc.close()
     vocabulary.close()
@@ -85,34 +69,29 @@ def search_words_orthographic_vocabulary(text):
     """
     This function search neologisms from the text on the vocabularies site and give list of neologisms.
     """
-    list_of_neologisms = []
     for word in text:
         page = urllib.request.urlopen('http://slovnyk.ua/?swrd={}'.format(quote(word))).read()
         soup = BeautifulSoup(page, 'html.parser')
-        if not soup.find_all('table', class_='sfm_table'):
-            list_of_neologisms.append(word)
-    return list_of_neologisms
+        if not soup.find_all('div', class_='toggle-content'):
+            yield word
 
 
 def search_words_interpretative_vocabulary(text):
     """
     This function search neologisms from the text on the vocabularies site and give list of neologisms.
     """
-    list_of_neologisms = []
     for word in text:
         translit = ''.join(TRANSLIT[letter] for letter in word)
         page = urllib.request.urlopen('http://sum.in.ua/s/{}'.format(quote(translit))).read()
         soup = BeautifulSoup(page, 'html.parser')
         if soup.find_all('div', id='search-res'):
-            list_of_neologisms.append(word)
-    return list_of_neologisms
+            yield word
 
 
 def search_words_internet_vocabulary(text):
     """
     This function search neologisms from the text on the vocabularies site and give list of neologisms.
     """
-    list_of_neologisms = []
     for word in text:
         try:
             page = urllib.request.urlopen('http://ukrlit.org/slovnyk/{}'.format(quote(word))).read()
@@ -124,16 +103,7 @@ def search_words_internet_vocabulary(text):
             if err.code == 404:
                 pass
 
-        list_of_neologisms.append(word)
-    return list_of_neologisms
-
-
-def list_to_file(list_of_neologisms):
-    """
-    This function write list of neologisms to the file, that will be download by user.
-    """
-    with open('../neologism_file.txt', 'a', encoding="UTF-8") as neologism_file:
-        neologism_file.write(str(list_of_neologisms))
+        yield word
 
 
 def process(text):
@@ -144,4 +114,4 @@ def process(text):
     pre_word_list = search_words_own_vocabulary(first_text)
     word_list = search_words_orthographic_vocabulary(pre_word_list)
     word_list_two = search_words_interpretative_vocabulary(word_list)
-    return search_words_internet_vocabulary(word_list_two)
+    return list(search_words_internet_vocabulary(word_list_two))
